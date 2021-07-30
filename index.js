7d32bd7214952d90900657da81485bc579e6c647
putra nugroho <putra.nugroho@alodokter.com>
2021-07-30 14:07:50 +0700

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-var crypto = require('crypto')
 var express = require('express')
 var bodyParser = require('body-parser')
-var axios = require('axios')
+var mysql = require('mysql')
+
+//CONNECT TO MYSQL
+var conn = mysql.createConnection({
+    host:'us-cdbr-east-03.cleardb.com',
+    user:'b1ac05f9bac715',
+    password:'de7cb19a',
+    database:'heroku_c21ba68034e1281'
+})
 
 var app=express()
 app.use(bodyParser.json())
@@ -13,90 +20,40 @@ app.listen(port,()=>{
     console.log(`You're Connected in port : ${port}`);
 })
 
-var sha256 = function (signature,secret_key) {
-    var hash = crypto.createHmac('sha256',secret_key)
-    hash.update(signature)
-    var value = hash.digest('hex')
-    return value
-    
-}
-
 app.get("/",(req,res,next)=>{
     res.end(JSON.stringify(`Welcome to Putra Nugroho [Absensi] RestAPI port : ${port}`))
 })
 
-app.post("/check_saldo",(req,res,next)=>{
+// CREATE CART
+app.post('/addOrder', (req, res) => {
+    const sql = `INSERT INTO order (pemesan, pasien, detail_pesanan)
+                VALUES ( '${req.body.pemesan}', '${req.body.pasien}', '${req.body.detail_pesanan}' )`
 
-    const clientID = "ATMPNN"
-    const ClientSecret = `52TK3zfB")feeX};`
-    const timestamp = req.body.timestamp
-    const mti = req.body.mti
-    const kd = req.body.trn_code
-    const userid = req.body.termid
-    const trcid = req.body.rrn
-    const data = req.body.data
-    const card_no = req.body.data.card_no
-    const amount = req.body.data.amount
-    const trace_id = req.body.data.trace_id
-    const time_trn = req.body.data.time_trn
-    const date_trn = req.body.data.date_trn
-    const merch_type = req.body.data.merch_type
-    const pos_entry = req.body.data.pos_entry
-    const network = req.body.data.network
-    const acquirer_id = req.body.data.acquirer_id
-    const merch_id = req.body.data.merch_id
-    const location = req.body.data.location
-    const iss_acct_dr = req.body.data.iss_acct_dr
+    conn.query(sql, (err,result) => {
+        if (err) return res.send(err)
 
-    const response = {
-        mti: mti,
-        trn_code: kd,
-        termid: userid,
-        timestamp: timestamp,
-        rrn: trcid,
-        rcode: '00',
-        rc_desc: 'SUKSES',
-        data: {    
-            card_no : card_no,
-            trn_code : kd,
-            amount : amount,
-            trace_id : trace_id,
-            time_trn : time_trn,
-            date_trn : date_trn,
-            merch_type : merch_type,
-            pos_entry : pos_entry,
-            network : network,
-            acquirer_id : acquirer_id,
-            location : location,
-            iss_acct_dr : iss_acct_dr,
-            acct_balance : `150000000.00`
-        }
-        }
+        res.send(result)
+    })
+})
 
-    res.end(JSON.stringify(response))
-    // const body = `{"kd":"8001","userid":"ATM01","trcid":"20210531094415","data":{"noacc":"1100100023"}}`
-    // const signature = clientID+body+timestamp
-    // const secret_key = ClientSecret+trcid
+router.get('/history', (req, res) => {
+    const sql = `select * from order;`
 
-    // const api_signature = sha256(signature,secret_key)  
+    conn.query(sql,(err, result) => {
+        if(err) return res.send(err)
 
-    // axios({
-    //     method: 'post',
-    //     url: 'http://103.101.225.212:12413/mgp/api-atm',
-    //     headers: {
-    //         'api-signature': 'a594fb1651356e3ef5f8c32f4d779212717ea2c9d5d9fbb98060a499e4aa0e57',
-    //         'api-timestamp': '20210723152334'
-    //         },
-    //     data: {
-    //         "kd": "8001",
-    //         "userid": "ATM01",
-    //         "trcid": "20210531094415",
-    //         "data": {
-    //             "noacc": "1100100023"
-    //         }
-    //     }
-    // }).then(function (response) {
-    //     console.log(response.data)
-    //     res.end(JSON.stringify(response.data))
-    // });
+        res.send(result)
+    })
+})
+
+router.get('/detail_history/:trans_id', (req, res) => {
+    const sql = `SELECT * FROM order
+                WHERE trans_id = ?`
+    const data = req.params.trans_id
+
+    conn.query(sql, data, (err, result) => {
+        if(err) return res.send(err)
+
+        res.send(result)
+    })
 })
